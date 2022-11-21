8480cfaedde942e56430e8c27f2c2462af00c7bd
Zhaohan Dong <zhaohan_dong@CR3-Flt-Computer.local>
2022-11-21 16:07:21 +0000

    Deleted old file

--- strlwr.h
+++
@@ -1,4 +0,0 @@
-#include <ctype.h>
-#include <string.h>
-
-char* strlwr(char* s);
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include "query.h"
-
-int main() {
-    gethttps("https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString=KDEN&hoursBeforeNow=2");
-}
